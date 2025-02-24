c598c893fa4ab0d63cd84f20f764026ee2d8080e
Nariman <sailaubek.nar@gmail.com>
2025-02-24 17:43:32 +0500

    Dump

--- client.php
+++ client.php
@@ -14,18 +14,10 @@ $worker = new Worker();
 $worker->onWorkerStart = function (): void {
     $userlogin = null;
     $messages = new FixedQueue(50);
+
     $inputLine = "";
-    $redraw = function () use ($messages, &$inputLine, &$userlogin): void {
-        echo "\033[H\033[J";
-
-        foreach ($messages->getQueue() as $msg) {
-            echo $msg . PHP_EOL;
-        }
-
-        $userlogin = $userlogin ?? 'you';
-
-        echo "$userlogin: " . $inputLine . "\033[K";
-    };
+    $messageBuffer = "";
+    $blocking = false;
 
     $remote = readline('pls enter remote address: ');
     $chat = new AsyncTcpConnection('ws://' . $remote . ':' . PORT, [
@@ -37,11 +29,11 @@ $worker->onWorkerStart = function (): void {
 
     $chat->onConnect = function (TcpConnection $tcpConnection): void {
         _log('Connection with remote chat established');
+        _log('[WAIT FOR LOGIN]');
     };
 
-    $chat->onMessage = function (TcpConnection $tcpConnection, string $data) use (&$messages, $redraw, &$userlogin): void {
+    $chat->onMessage = function (TcpConnection $tcpConnection, string $data) use (&$messages, &$userlogin, &$inputLine, &$messageBuffer, &$blocking): void {
         $msg = Msg::fromArray(json_decode($data, true));
-
         switch ($msg->action) {
             case Action::ReqLogin:
                 $userlogin = readline('Please, input your login: ');
@@ -57,24 +49,83 @@ $worker->onWorkerStart = function (): void {
 
             case Action::Message:
                 $author = $msg->author ?? 'anonymus' ;
-                $messages->push($author . ': ' . $msg->data);
-                $redraw();
+                $messageBuffer = $messageBuffer . "\n". ($author . ': ' . $msg->data);
                 break;
             case Action::History:
                 foreach (json_decode($msg->data, true) as $mes) {
-                    $messages->push($mes);
+                    $messageBuffer = $messageBuffer . "\n" . $mes;
                 };
-                $redraw();
                 stream_set_blocking(STDIN, false);
-                Timer::add(0.1, function () use ($tcpConnection): void {
-                    $input = fgets(STDIN);
-                    if ($input !== false) {
-                        $input = trim($input);
-                        if (!empty($input)) {
-                            echo "\033[H";
-                            $tcpConnection->send(strval(new Msg(Action::Message, $input)));
+                system("stty -icanon -echo");
+                _log('[CHAT STARTED]');
+                Timer::add(0.05, function () use ($tcpConnection, &$inputLine, &$messageBuffer, &$blocking): void {
+                    $msgBuffArr = explode("\n", $messageBuffer);
+                    if (count($msgBuffArr) > 1) {
+                        $starting = 0;
+                        foreach ($msgBuffArr as $index => $message) {
+                            if ($message === '') {
+                                if ($index === $starting) {
+                                    $starting++;
+                                }
+                                continue;
+                            }
+
+                            if ($blocking) {
+                                echo "\n";
+                                $blocking = false;
+                            }
+
+                            if ($index === $starting) {
+                                echo "\r" . str_repeat(' ', strlen($inputLine) + 5);
+                                echo  "\r" . $message ;
+                                continue;
+                            }
+
+                            echo  "\n" . $message ;
+                        }
+                        $messageBuffer = '';
+                        echo "\n";
+                        if (!empty($inputLine)) {
+                            echo "You: " . $inputLine;
+                        }
+                        return;
+                    }
+
+                    $c = fread(STDIN, 10);
+                    if ($c === false || $c === '') {
+                        return;
+                    }
+                    if ($c[0] === "\n") {
+                        $tcpConnection->send((new Msg(Action::Message, $inputLine))->__tostring());
+                        $inputLine = "";
+                        $blocking = true;
+                        return;
+                    }
+                    $inputLine = $inputLine . $c;
+
+                    $cc = strlen($inputLine);
+
+                    $processedInput = "";
+                    $backspaceCount = 0;
+
+                    for ($i = strlen($inputLine) - 1; $i >= 0; $i--) {
+                        if ($inputLine[$i] === chr(127)) {
+                            $backspaceCount++;
+                        } elseif ($backspaceCount > 0) {
+                            $backspaceCount--;
+                        } else {
+                            $processedInput = $inputLine[$i] . $processedInput;
                         }
                     }
+
+                    $inputLine = $processedInput;
+
+                    if ($blocking) {
+                        echo "\n";
+                        $blocking = false;
+                    }
+                    echo "\r" . str_repeat(" ", $cc + 5 - $backspaceCount);
+                    echo "\rYou: " . $inputLine;
                 });
                 break;
             default:
@@ -84,13 +135,28 @@ $worker->onWorkerStart = function (): void {
     };
 
     $chat->onClose = function (): void {
-        _log('Connection closed');
         stream_set_blocking(STDIN, true);
-        Worker::stopAll();
+        system("stty sane");
     };
 
     $chat->connect();
 };
 
 
+$worker->onWorkerStop = function (): void {
+    stream_set_blocking(STDIN, true);
+    system("stty sane");
+};
+
+$worker->onClose = function (): void {
+    stream_set_blocking(STDIN, true);
+    system("stty sane");
+};
+
 Worker::runAll();
+
+
+(function (): void {
+    stream_set_blocking(STDIN, true);
+    system("stty sane");
+})();
